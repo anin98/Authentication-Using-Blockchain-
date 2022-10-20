2dbb7ce241d32cf66549a7296a122514008dfa0b
Anindita <anindita@Aninditas-MacBook-Air.local>
2022-10-20 15:06:07 +0600

    ninth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,41 +4,6 @@ import {API_BASE_URL} from './config';
 import './App.css';
 import Blockchain from './Blockchain';
 function App() {
-const [nameReg,setNameReg] = useState('')
-const [emailReg,setEmailReg] = useState('')
-const [passwordReg,setPasswordReg] = useState('')
-const register =() => {
-Axios.post('http://localhost3000/register',{name:
-nameReg,
-email:emailReg,
-password:passwordReg,
-}).then((response)=> {
-console.log(response);
-});
-};
-return (
-<div className ="Registration">
-<h1>Registration</h1>
-<label>Name</label>
-<input type ="text" onChange={(e) => {setNameReg(e.target.value);
- }}/>
-<label>Email</label>
-<input type ="text" onChange={(e) => {setEmailReg(e.target.value);
- }}/>
-<label>Password</label>
-<input type ="text" onChange={(e) => {setPasswordReg(e.target.value);
- }}
-  />
-<button onClick={register}> Register </button>
-
-<div className="login">
-<h1>Login</h1>
-<input type ="text" placeholder ="Username"/>
-<input type ="text" placeholder ="Password"/>
-<button> Login </button>
-</div>
-</div>
-);
 
 
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+var port =3700;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,30 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-const express = require("express");
-const mysql = require("mysql");
 
-const app = express();
-app.use(express.json());
 
-const db = mysql.createConnection({
-user: "root",
-host: "localhost",
-password: "password",
-database:"blockchain_authentication",
-});
-app.post('/register',(req,res)=> {
-const name = req.body.name
-const email = req.body.email
-const password = req.body.password
-db.query("INSERT INTO registration (name,email,password) VALUES (?,?,?)",
-[name,email,password],
-(err,result) => {
-console.log(err);
-}
-);
-});
-app.listen(3000,() => {
-console.log("running server");
-});
 
