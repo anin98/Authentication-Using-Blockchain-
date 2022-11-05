5c0d3eb832f7f4b208fe6736b33f7bd63350ae8d
Sofia Safrina <sofiasafrina@Sofias-MacBook-Pro.local>
2022-11-06 00:29:02 +0600

    Sofia's correct branch

--- cliyent/src/App.js
+++ cliyent/src/App.js
@@ -1,26 +1,22 @@
-import Register from './Register';
-import Login from './Login';
-import Layout from './Layout';
-import RequireAuth from './RequireAuth';
-import Home from './Home';
-import {Routes, Route} from 'react-router-dom';
+import Register from "./Register";
+import Login from "./Login";
+import Layout from "./Layout";
+import RequireAuth from "./RequireAuth";
+import Home from "./Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <Routes>
-      <Route path ="/" element ={<Layout/>}>
+      <Route path="/" element={<Layout />}>
         {/*public routes */}
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
 
         {/*protected routes */}
-        
-        <Route path ="home" element={<Home/>} />
-        </Route>
 
-
-      
+        <Route path="/home" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
