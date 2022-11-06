01ff3737a8c0100602511afa3ece443b9d9f4655
dbkhurshid <67158657+dbkhurshid@users.noreply.github.com>
2022-11-06 07:50:13 +0600

    Dishari's 2nd + correct branch with front end changes

--- cliyent/src/App.js
+++ cliyent/src/App.js
@@ -1,21 +1,34 @@
-import Register from "./Register";
-import Login from "./Login";
-import Layout from "./Layout";
-import RequireAuth from "./RequireAuth";
-import Home from "./Home";
-import { Routes, Route } from "react-router-dom";
+import Register from './Register';
+import Login from './Login';
+import Layout from './Layout';
+import RequireAuth from './RequireAuth';
+import Home from './Home';
+import {Routes, Route} from 'react-router-dom';
+
+import ErrorPage from './ErrorPage';
+import Default from './Default';
+
 
 function App() {
+
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path ="/" element ={<Layout/>}>
+     
         {/*public routes */}
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-
+        <Route path="/" element={<Default/>}/>
+       
+        {/* <Route path ="/home" element={<Home/>} /> */}
+       
         {/*protected routes */}
+        <Route element ={<RequireAuth/>}>
+          <Route path ="/" element={<Home/>} />
+        </Route>
 
-        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
+
       </Route>
     </Routes>
   );
--- cliyent/src/Home.js
+++ cliyent/src/Home.js
@@ -1,71 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
-import AuthContext from "./context/AuthProvider";
-import React from "react";
-import axios from "axios";
-
-const baseURL = process.env.REACT_APP_API_URL;
+import {useNavigate, Link} from "react-router-dom"
+import { useContext } from "react"
+import AuthContext from "./context/AuthProvider"
+import React from 'react'
 
 const Home = () => {
-  // const { setAuth } = useContext(AuthContext);
-  const navigate = useNavigate();
+    const {setAuth} = useContext(AuthContext);
+    const navigate =useNavigate();
 
-  const [user, setUser] = useState(null);
+    const logout = async () => {
 
-  // -> at initial render, we need to fetch the user details by sending
-  // -> request to '/@me' route
-  // -> if we have the user data in api response
-  // -> we'll show the user data in ui, else
-  // -> we'll redirect to login page
-
-  useEffect(() => {
-    // fetch the user here
-    const getuserData = async () => {
-      try {
-        const response = await axios.get(`${baseURL}/@me`, {
-          withCredentials: true,
-        });
-        setUser(response.data);
-      } catch (error) {
-        if (axios.isAxiosError(error) && error.response) {
-          if (error.response.status === 401) {
-            navigate("/login");
-          }
-        }
-      }
-    };
-
-    getuserData();
-  }, []);
-
-  console.log(user);
-
-  const logout = async () => {
-    await axios.post(`${baseURL}/logout`, { withCredentials: true });
-    navigate("/login");
-  };
-
+        setAuth({});
+        navigate('/login');
+    }
   return (
+    
     <section>
-      <div>Home</div>
-
-      {user && (
-        <div>
-          <p>Email: {user.email}</p>
-          <p>Name: {user.name}</p>
-          <p>ID: {user.id}</p>
-        </div>
-      )}
-
-      <br />
-      <p>You are logged in!</p>
-      {/* <span>Count: {counter}</span>
-      <button onClick={() => setCounter(counter + 1)}>Increae Me</button> */}
-      <div className="flexGrow">
+    <div>Home</div>
+    <br />
+    <p>You are logged in!</p>
+    <div className="flexGrow">
         <button onClick={logout}>Sign Out</button>
-      </div>
+    </div>
     </section>
-  );
-};
+  )
+}
 
-export default Home;
+
+export default Home
